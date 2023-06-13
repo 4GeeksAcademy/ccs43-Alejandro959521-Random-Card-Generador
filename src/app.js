d494a126d12dc79b3226885c6d94a99a91f77132
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-06-13 19:10:00 +0000

    Agregado de periquitos por presion social

--- src/app.js
+++ src/app.js
@@ -30,3 +30,31 @@ window.onload = function() {
     document.querySelector(".simbolo2").style.color = "black";
   }
 };
+
+document.querySelector(".cambialo").addEventListener("click", Cambialo);
+
+function Cambialo() {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  let numerosC = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "Q", "K", "A"];
+  let simbolosC = ["♣", "♥", "♠", "♦"];
+
+  let numeroA = getRandomInt(simbolosC.length);
+  document.querySelector(".simbolo1").innerHTML = simbolosC[numeroA];
+
+  document.querySelector(".simbolo2").innerHTML = simbolosC[numeroA];
+
+  document.querySelector(".numeros").innerHTML =
+    numerosC[getRandomInt(numerosC.length)];
+
+  if (simbolosC[numeroA] == "♦" || simbolosC[numeroA] == "♥") {
+    document.querySelector(".simbolo1").style.color = "red";
+    document.querySelector(".simbolo2").style.color = "red";
+  } else {
+    document.querySelector(".simbolo1").style.color = "black";
+    document.querySelector(".simbolo2").style.color = "black";
+  }
+}
+setInterval(Cambialo, 10000);
